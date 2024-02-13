9c34161306bbf7a638b8a29de972d87c8e5041f2
VennelaBoda <bvennela753@gmail.com>
2024-02-13 23:54:30 +0530

    day-22

---
+++ Longest_Subarray_With_Sum_K_(positives).cpp
@@ -0,0 +1,41 @@
+int longestSubarrayWithSumK(vector<int> a, long long k) {
+
+    int maxLen= 0;
+
+    int left = 0, right = 0;
+
+    long long sum = a[0];
+
+    int n = a.size();
+
+    while (right < n) {
+
+        while (left <= right && sum > k) {
+
+            sum -= a[left];
+
+            left++;
+
+        }
+
+
+        if (sum == k) {
+
+            maxLen=max(maxLen,right-left+1);
+
+        }
+
+        right++;
+
+        if (right < n) {
+
+            sum += a[right];
+
+        }
+
+    }
+
+    return maxLen;
+
+    
+}
